00f6afc8b8ff45e2b771f6b6dd572c927b7245b1
Gabriel <gabrieljoschem@gmail.com>
2026-01-05 17:37:38 -0300

    Começando Projeto2

---
+++ API_Recebe_Dados/src/main/java/com/projeto2/api_recebe_dados/ApiRecebeDadosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto2.api_recebe_dados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRecebeDadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRecebeDadosApplication.class, args);
+    }
+
+}
---
+++ API_Recebe_Dados/src/main/java/com/projeto2/api_recebe_dados/controller.java
@@ -0,0 +1,4 @@
+package com.projeto2.api_recebe_dados;
+
+public class controller {
+}
