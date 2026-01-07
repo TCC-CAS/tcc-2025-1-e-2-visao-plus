f4888545666e8f2bea30b64e8d0cebe63bd9c67d
Gabriel <gabrieljoschem@gmail.com>
2026-01-07 01:06:37 -0300

    Lógica de login adicionada no backend.

--- API_Banco/src/main/java/com/Gabriel/API_Banco/controller/UsuarioController.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/controller/UsuarioController.java
@@ -3,7 +3,10 @@ package com.Gabriel.API_Banco.controller;
 import com.Gabriel.API_Banco.model.Usuario;
 import com.Gabriel.API_Banco.service.UsuarioService;
 import com.Gabriel.API_Banco.service.UsuarioService;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -20,6 +23,24 @@ public class UsuarioController {
     public String registrarUsuario(@RequestBody Usuario usuario){
         s.salvar(usuario);
         return "Usuário " + usuario.getNome() + " salvo com sucesso!";
+    }
+
+    @GetMapping("/login")
+    public String loginUsuario(@RequestBody Usuario usuario){
+        Optional<Usuario> usuarioNoBanco = s.consultarPorEmail(usuario.getEmail());
+
+        if(usuarioNoBanco.isPresent()){
+            String senhaNoBanco = usuarioNoBanco.get().getSenha();
+
+            // Compara com a senha que veio no login
+            if(usuario.getSenha().equals(senhaNoBanco)){
+                return "Login realizado com sucesso!";
+            } else {
+                return "Senha inválida!";
+            }
+        }else{
+            return "Usuário não encontrado!";
+        }
     }
 
 }
