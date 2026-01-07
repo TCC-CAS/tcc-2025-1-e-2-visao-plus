69eee51114a7cc3130edf9273c04fca7572db263
Gabriel <gabrieljoschem@gmail.com>
2026-01-06 23:33:11 -0300

    CrossOrigin para generalizar o acesso à API

--- API_Banco/src/main/java/com/Gabriel/API_Banco/controller/UsuarioController.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/controller/UsuarioController.java
@@ -3,12 +3,9 @@ package com.Gabriel.API_Banco.controller;
 import com.Gabriel.API_Banco.model.Usuario;
 import com.Gabriel.API_Banco.service.UsuarioService;
 import com.Gabriel.API_Banco.service.UsuarioService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
