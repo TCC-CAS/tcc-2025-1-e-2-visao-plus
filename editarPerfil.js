e67fe7b1234ea6887e17e4b2af7b19f2ac45fde6
Gabriel <gabrieljoschem@gmail.com>
2026-01-14 04:02:30 -0300

    Função editarUsuario modificada.

--- editarPerfil.js
+++ editarPerfil.js
@@ -65,15 +65,51 @@ async function salvarPerfil(e) {
 
     const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
 
-    const dadosAtualizados = {
+    const dadosUsuario = {
         id: usuario.id,
         nome: document.getElementById("edit-nome-usuario").value,
         email: document.getElementById("edit-email-usuario").value,
         senha: document.getElementById("edit-senha-usuario").value
     };
 
-    console.log("Salvar perfil:", dadosAtualizados);
+    console.log("DTO Perfil:", dadosUsuario);
 
+    const usuarioAtualizado = await editarDadosUsuario(dadosUsuario);
+
+    if (usuarioAtualizado) {
+        dadosUsuario = usuarioAtualizado;
+        // Atualiza o localStorage com os novos dados do usuário
+        localStorage.setItem("usuarioLogado", JSON.stringify(usuarioAtualizado));
+        alert("Perfil atualizado com sucesso!");
+        document.getElementById("modal-editar-perfil").classList.add("hidden");
+    } else {
+        alert("Erro ao atualizar perfil. Tente novamente.");
+    }
 }
 
+// ===== API =====
 
+async function editarDadosUsuario(dadosUsuario) {
+    try {
+        const response = await fetch("http://localhost:8080/usuarios/editarUsuario", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dadosUsuario)
+        });
+        if (!response.ok) return null;
+
+        return await response.json();
+    } catch (error) {
+        console.error("Erro ao editar usuário:", error);
+        return null;
+    }
+}
+
+// ===== LOCAL STORAGE =====
+
+function atualizarLojaNoLocalStorage(usuarioAtualizado) {
+    const usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
+    localStorage.setItem("usuarioLogado", JSON.stringify(usuarioAtualizado));
+}
