b0211ddfdbc6805dcee49f493bea5186fc5462df
Gabriel <gabrieljoschem@gmail.com>
2026-01-19 23:16:59 -0300

    Mudança no service para deletar o usuário.

--- API_Banco/src/main/java/com/Gabriel/API_Banco/service/UsuarioService.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/service/UsuarioService.java
@@ -5,18 +5,24 @@ import java.util.Optional;
 
 import com.Gabriel.API_Banco.dto.EditarUsuarioDTO;
 import com.Gabriel.API_Banco.dto.ListarUsuariosDTO;
+import com.Gabriel.API_Banco.repository.LojaRepositorio;
 import org.springframework.stereotype.Service;
 import com.Gabriel.API_Banco.model.Usuario;
 import com.Gabriel.API_Banco.repository.UsuarioRepositorio;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UsuarioService {
 
     private final UsuarioRepositorio r;
+    private final LojaRepositorio lr;
 
-    public UsuarioService(UsuarioRepositorio r) {
+    public UsuarioService(UsuarioRepositorio r, LojaRepositorio lr) {
         this.r = r;
+        this.lr = lr;
     }
+
+
 
     public Usuario salvar(Usuario usuario) {
         return r.save(usuario);
@@ -49,11 +55,17 @@ public class UsuarioService {
                 .toList();
     }
 
+    @Transactional
     public void deletarUsuario(Long id) {
         Usuario usuario = r.findById(id)
                 .orElseThrow(() -> new RuntimeException("Usuário não encontrado!"));
 
+        // Se for vendedor, deletar loja primeiro
+        if (usuario.getTipoUsuario() == TipoUsuario.VENDEDOR) {
 
+            lojaRepository.findByDonoId(id)
+                    .ifPresent(lojaRepository::delete);
+        }
 
         r.delete(usuario);
     }
