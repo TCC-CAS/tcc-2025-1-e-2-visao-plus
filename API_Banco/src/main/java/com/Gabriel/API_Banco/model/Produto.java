a3a6f3403e27df98326b3f506fc0c8cbe433e861
Gabriel <gabrieljoschem@gmail.com>
2026-01-20 23:43:38 -0300

    Modificando entidade produto para atribuir ao usuário

--- API_Banco/src/main/java/com/Gabriel/API_Banco/model/Produto.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/model/Produto.java
@@ -1,14 +1,7 @@
 package com.Gabriel.API_Banco.model;
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -37,5 +30,8 @@ public class Produto {
 
     @Column(name = "prazo_entrega_dias")
     private Integer prazoEntregaDias;
+
+    OneToMany
+
 }
 
