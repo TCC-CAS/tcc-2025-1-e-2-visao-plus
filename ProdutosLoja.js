2c6a00ccb3664663b7363bd9f04f9e8668038227
Gabriel <gabrieljoschem@gmail.com>
2026-01-24 18:52:37 -0300

    funções de chamada de API

--- ProdutosLoja.js
+++ ProdutosLoja.js
@@ -1,12 +1,38 @@
-const API_BASE_URL = "http://localhost:8080/"; // ajusta se precisar
+const API = "http://localhost:8080/"; // ajusta se precisar
 
 document.addEventListener("DOMContentLoaded", () => {
     initPaginaAdmin();
 });
 
 function initPaginaAdmin() {
-    setNomeLoja("Ótica XYZ"); // depois você pode buscar isso do backend
+    setNomeLoja(Nomeloja); // depois você pode buscar isso do backend
     carregarArmacoes();
     carregarLentes();
     configurarEventos();
+}
+
+async function setNomeLoja() {
+    try {
+        const response = await fetch(`${API}/lojas/buscarLoja`);
+        if (!response.ok) throw new Error("Erro ao buscar loja");
+
+        const loja = await response.json();
+        const Nomeloja = loja.nome;
+    } catch (error) {
+        console.error(error);
+        alert("Erro ao carregar lojas");
+    }
+}
+
+async function carregarArmacoes() {
+    try {
+        const response = await fetch(`${API}/armacoes/listarArmacoes`);
+        if (!response.ok) throw new Error("Erro ao buscar armações");
+
+        const armacoes = await response.json();
+        renderizarArmacoes(armacoes);
+    } catch (error) {
+        console.error(error);
+        alert("Erro ao carregar armações");
+    }
 }
