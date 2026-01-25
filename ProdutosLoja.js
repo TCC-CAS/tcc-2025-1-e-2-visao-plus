03c7f1bbb4569c2900cf19d368a62239078c1e6f
Gabriel <gabrieljoschem@gmail.com>
2026-01-24 22:16:27 -0300

    Funções de setar o nome da loja, criação de cards de lente e armações.

--- ProdutosLoja.js
+++ ProdutosLoja.js
@@ -24,6 +24,8 @@ async function setNomeLoja() {
     }
 }
 
+//FUNÇÕES DE CARREGAMENTO DE API'S - ARMAÇÕES E LENTES
+
 async function carregarArmacoes() {
     try {
         const response = await fetch(`${API}/armacoes/listarArmacoes`);
@@ -50,6 +52,8 @@ async function carregarLentes() {
     }
 }
 
+//FUNÇÕES DE MODAIS
+
 function abrirModal(idModal) {
     document.getElementById(idModal).classList.remove("hidden");
 }
@@ -58,32 +62,72 @@ function fecharModal(idModal) {
     document.getElementById(idModal).classList.add("hidden");
 }
 
+//FUNÇÕES DE RENDERIZAÇÃO DE ARMAÇÕES
+
 function renderizarArmacoes(armacoes) {
     const container = document.getElementById("lista-armacoes");
     container.innerHTML = "";
+
+    if (armacoes.length === 0) {
+        container.innerHTML = "<p>Nenhuma Armação cadastrada.</p>";
+        return;
+    }
+
     armacoes.forEach(armacao => {
-        const div = document.createElement("div");
-        div.className = "produto";
-        div.innerHTML = `
-            <h3>${armacao.nome}</h3>
-            <p>Preço: R$ ${armacao.preco}</p>
-            <button onclick="abrirModal('modal-editar-armacao')">Editar</button>
-        `;
-        container.appendChild(div);
+        const cardArmacao = CardArmacao(armacao);
+        container.appendChild(cardArmacao);
     });
 }
+
+function CardArmacao(armacao) {
+    const div = document.createElement("div");
+    div.classList.add("card-armacao");
+
+    div.innerHTML = `
+        <h3>${armacao.nome}</h3>
+        <p>Marca: ${armacao.marca}</p>
+        <p>Modelo: ${armacao.modelo}</p>
+        <p>Material: ${armacao.material}</p>
+        <p>Tipo: ${armacao.tipo}</p>
+        <p>Descrição: ${armacao.descricao}</p>
+        <p>Preço: R$ ${armacao.preco}</p>
+        <button onclick="abrirModal('modal-editar-armacao')">Editar</button>
+        <button onclick="abrirModal('modal-deletar-armacao')">Deletar</button>
+    `;
+    return div;
+}
+
+//FUNÇÕES DE RENDERIZAÇÃO DE LENTES
 
 function renderizarLentes(lentes) {
     const container = document.getElementById("lista-lentes");
     container.innerHTML = "";
+    
+    if (lentes.length === 0) {
+        container.innerHTML = "<p>Nenhuma Lente cadastrada.</p>";
+        return;
+    }
+
     lentes.forEach(lente => {
-        const div = document.createElement("div");
-        div.className = "produto";
-        div.innerHTML = `
-            <h3>${lente.nome}</h3>
-            <p>Preço: R$ ${lente.preco}</p>
-            <button onclick="abrirModal('modal-editar-lente')">Editar</button>
-        `;
-        container.appendChild(div);
+        const cardLente = CardLente(lente);
+        container.appendChild(cardLente);
     });
+}
+
+function CardLente(lente) {
+    const div = document.createElement("div");
+    div.classList.add("card-lente");
+
+    div.innerHTML = `
+        <h3>${lente.nome}</h3>
+        <p>Tipo: ${lente.tipo}</p>
+        <p>Marca: ${lente.marca}</p>
+        <p>Modelo: ${lente.modelo}</p>
+        <p>Material: ${lente.material}</p>
+        <p>Descrição: ${lente.descricao}</p>
+        <p>Preço: R$ ${lente.preco}</p>
+        <button onclick="abrirModal('modal-editar-lente')">Editar</button>
+        <button onclick="abrirModal('modal-deletar-lente')">Deletar</button>
+    `;
+    return div;
 }
