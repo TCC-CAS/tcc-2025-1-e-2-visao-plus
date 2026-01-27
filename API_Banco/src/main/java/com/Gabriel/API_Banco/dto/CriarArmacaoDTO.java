a5fc643d3a02634104969fb6b7236869856c5562
Gabriel <gabrieljoschem@gmail.com>
2026-01-26 23:48:54 -0300

    Começando as DTO's de Armação

--- API_Banco/src/main/java/com/Gabriel/API_Banco/dto/CriarArmacaoDTO.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/dto/CriarArmacaoDTO.java
@@ -1,4 +1,13 @@
 package com.Gabriel.API_Banco.dto;
 
 public class CriarArmacaoDTO {
+
+    private String nome;
+    private String tipo;
+    private String marca;
+    private String modelo;
+    private String material;
+    private String descricao;
+    private Double preco;
+    private Long idLoja
 }
