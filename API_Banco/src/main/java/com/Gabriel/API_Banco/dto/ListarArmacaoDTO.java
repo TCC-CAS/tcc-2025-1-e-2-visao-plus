1a4e167462d4ded4d7e7f8d52ca71c13cab275d4
Gabriel <gabrieljoschem@gmail.com>
2026-01-26 23:55:16 -0300

    Finalizando dto de listagem de armação

--- API_Banco/src/main/java/com/Gabriel/API_Banco/dto/ListarArmacaoDTO.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/dto/ListarArmacaoDTO.java
@@ -35,4 +35,62 @@ public class ListarArmacaoDTO {
         this.descricao = descricao;
         this.preco = preco;
     }
+
+    // getters e setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMaterial(){
+        return material;
+    }
+    public void setMaterial(String material){
+        this.material = material;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
 }
