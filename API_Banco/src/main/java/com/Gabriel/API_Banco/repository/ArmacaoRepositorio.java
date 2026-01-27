9b3b14eced472b606fb9ca0cc86af9d161540cc4
Gabriel <gabrieljoschem@gmail.com>
2026-01-27 00:00:40 -0300

    Edição repositório de armação

--- API_Banco/src/main/java/com/Gabriel/API_Banco/repository/ArmacaoRepositorio.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/repository/ArmacaoRepositorio.java
@@ -3,8 +3,10 @@ package com.Gabriel.API_Banco.repository;
 import com.Gabriel.API_Banco.model.Armacao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArmacaoRepositorio extends JpaRepository<Armacao, Long> {
 
-
+    List<Armacao> findByLoja_Id(Long idLoja);
 
 }
