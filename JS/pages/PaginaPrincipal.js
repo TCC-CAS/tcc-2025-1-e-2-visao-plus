b30bd86b0dd5f2135617e825ec6cee635eebb445
Gabriel <gabrieljoschem@gmail.com>
2026-02-11 17:59:02 -0300

    Criação e chamada dos métodos de listagem de lojas e organização da página principal!!

--- JS/pages/PaginaPrincipal.js
+++ JS/pages/PaginaPrincipal.js
@@ -1,6 +1,7 @@
 import { configurarHeader } from "../components/header.js";
 import { getUsuarioLogado } from "../core/auth.js";
 import { getLojaAtual } from "../core/loja.js";
+import { listarLojas } from "../core/loja.js";
 
 
 function esconderTodosOsBlocos() {
@@ -36,9 +37,48 @@ function configurarTela() {
     }
 }
 
+async function carregarLojas() {
+    try {
+        const lojas = await listarLojas();
+        renderizarLojas(lojas);
+    } catch (error) {
+        console.error(error);
+        alert("Erro ao carregar lojas");
+    }
+}
+
+function renderizarLojas(lojas) {
+    const container = document.getElementById("lista-lojas");
+    container.innerHTML = "";
+
+    if (lojas.length === 0) {
+        container.innerHTML = "<p>Nenhuma loja cadastrada.</p>";
+        return;
+    }
+
+    lojas.forEach(loja => {
+        const card = criarCardLoja(loja);
+        container.appendChild(card);
+    });
+}
+
+function criarCardLoja(loja) {
+    const div = document.createElement("div");
+    div.classList.add("card-loja");
+
+    div.innerHTML = `
+        <h2>${loja.nome}</h2>
+        <p><strong>${loja.email}</strong></p>
+        <p><strong>${loja.endereco}</strong></p>
+    `;
+
+    return div;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     configurarHeader();
     configurarTela();
+    carregarLojas();
 });
 
 
