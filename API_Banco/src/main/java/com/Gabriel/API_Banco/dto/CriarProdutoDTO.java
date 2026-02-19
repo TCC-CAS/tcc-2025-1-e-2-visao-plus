bd94ba4d5f10cd3303d6bf237fb450ed69303d86
Gabriel <gabrieljoschem@gmail.com>
2026-02-19 18:46:47 -0300

    Mudança no tipo do valor para a dto se adequar ao produto

--- API_Banco/src/main/java/com/Gabriel/API_Banco/dto/CriarProdutoDTO.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/dto/CriarProdutoDTO.java
@@ -2,9 +2,11 @@ package com.Gabriel.API_Banco.dto;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class CriarProdutoDTO {
     private Long idLoja;
     private String nome;
-    private Double valor;
+    private BigDecimal valor;
 }
