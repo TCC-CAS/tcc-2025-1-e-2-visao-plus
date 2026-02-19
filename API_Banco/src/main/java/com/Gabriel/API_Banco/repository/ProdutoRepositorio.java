ac43305e9654ea328c4ccaf18aa6a831b761fded
Gabriel <gabrieljoschem@gmail.com>
2026-02-19 18:48:42 -0300

    Criação da função de busca do produto por loja

--- API_Banco/src/main/java/com/Gabriel/API_Banco/repository/ProdutoRepositorio.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/repository/ProdutoRepositorio.java
@@ -1,9 +1,14 @@
 package com.Gabriel.API_Banco.repository;
+import com.Gabriel.API_Banco.model.Loja;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Gabriel.API_Banco.model.Produto;
 
+import java.util.List;
+
 
 public interface ProdutoRepositorio extends JpaRepository<Produto, Long> {
+
+    List<Produto> findByLoja(Loja loja);
 
 }
