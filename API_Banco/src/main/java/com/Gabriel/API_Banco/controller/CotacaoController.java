90729d9967a7d0f65eb2560387cc3c4599b2b40f
Gabriel <gabrieljoschem@gmail.com>
2026-02-20 17:53:11 -0300

    Criação da cotação no controller de cotação

--- API_Banco/src/main/java/com/Gabriel/API_Banco/controller/CotacaoController.java
+++ API_Banco/src/main/java/com/Gabriel/API_Banco/controller/CotacaoController.java
@@ -1,33 +1,22 @@
 package com.Gabriel.API_Banco.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Gabriel.API_Banco.dto.CriarCotacaoDTO;
+import com.Gabriel.API_Banco.model.Cotacao;
+import com.Gabriel.API_Banco.service.CotacaoService;
+import org.springframework.web.bind.annotation.*;
 
-import com.Gabriel.API_Banco.dto.CriarLojaDTO;
-import com.Gabriel.API_Banco.model.Loja;
-import com.Gabriel.API_Banco.service.LojaService;
-
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cotacoes")
 public class CotacaoController {
 
-    private final LojaService lojaService;
+    private final CotacaoService cotacaoService;
 
-    public CotacaoController(LojaService lojaService) {
-        this.lojaService = lojaService;
+    public CotacaoController(CotacaoService cotacaoService) {
+        this.cotacaoService = cotacaoService;
     }
 
     @PostMapping("/criarCotacao")
-    public ResponseEntity<Loja> criarLoja(@RequestBody CriarLojaDTO dto) {
-        Loja loja = lojaService.criarLoja(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(loja);
+    public Cotacao criar(@RequestBody CriarCotacaoDTO dto) {
+        return cotacaoService.criarCotacao(dto);
     }
-
-    
 }
