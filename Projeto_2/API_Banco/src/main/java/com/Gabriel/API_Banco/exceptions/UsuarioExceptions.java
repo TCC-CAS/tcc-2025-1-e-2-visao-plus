74b9e72d2650491f9669b3394ca943b2c6bc726f
Gabriel <gabrieljoschem@gmail.com>
2026-03-04 01:10:39 -0300

    Criação do método de exceptions messages

---
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/exceptions/UsuarioExceptions.java
@@ -0,0 +1,9 @@
+package com.Gabriel.API_Banco.exceptions;
+
+public class UsuarioExceptions extends RuntimeException {
+
+    public UsuarioExceptions(String mensagem) {
+        super(mensagem);
+    }
+
+}
