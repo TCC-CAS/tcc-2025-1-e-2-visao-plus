2b33d86373ea828ab8785f5a9d0e85fc700983cc
Gabriel <gabrieljoschem@gmail.com>
2026-03-08 00:20:04 -0300

    criação da lógica da página central da loja

---
+++ Projeto_2/JS/pages/CentralLoja.js
@@ -0,0 +1,6 @@
+import { configurarHeader } from "../components/header.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    configurarHeader();
+    // Outras inicializações específicas da página podem ser feitas aqui
+});
