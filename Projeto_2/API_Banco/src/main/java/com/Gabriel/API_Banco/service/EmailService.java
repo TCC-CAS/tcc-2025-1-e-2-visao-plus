d108dc2486ee216a4de1be093d07568755166808
Gabriel <gabrieljoschem@gmail.com>
2026-03-11 17:36:26 -0300

    criação das funções de recuperação de senha e confirmação de cotação

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/service/EmailService.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/service/EmailService.java
@@ -1,8 +1,11 @@
 package com.Gabriel.API_Banco.service;
+import com.Gabriel.API_Banco.model.Cotacao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class EmailService {
@@ -20,4 +23,45 @@ public class EmailService {
 
         mailSender.send(message);
     }
+
+    public void recuperacaoSenha(String para, String Link, String senha){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(para);
+        message.setSubject("Perdeu sua senha?");
+
+        message.setText(
+                "Olá!\n\n" +
+                        "Olá! Vimos que você esqueceu sua senha. Devido a isso viemos relembrá-la:\n" +
+                        senha +
+                        "Caso queira redefinir ela, clique no link abaixo:\n" +
+                        Link
+        );
+
+        mailSender.send(message);
+    }
+
+    public void criacaoDeCotacao(Cotacao cotacao){
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        message.setTo(cotacao.getUsuario().getEmail());
+        message.setSubject("Cotação Enviada!");
+
+        message.setText(
+                "Olá, "+ cotacao.getUsuario().getNome() +"!\n\n" +
+                        "Obrigado por utilizar da VisionPlus+ para realizar uma cotação!\n" +
+                        "Até o prazo: " +cotacao.getDataResposta().format(formatter) +
+                        "Estaremos acompanhando, para garantir de que seu pedido seja atendido!\n" +
+                        "Caso tenha algum comentário ou observação, sinta-se à vontade para usar o chat da cotação!\n"+
+                        "Muito obrigado, novamente, por confiar na VisionPlus+!"
+        );
+
+        mailSender.send(message);
+
+    }
+
+
 }
