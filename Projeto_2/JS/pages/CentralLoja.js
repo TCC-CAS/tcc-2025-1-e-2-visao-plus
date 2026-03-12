963ae323ba712dad77eff2e9b42e4811d08de566
Gabriel <gabrieljoschem@gmail.com>
2026-03-11 23:59:59 -0300

    Criação das funções de direcionamento para a página da loja

--- Projeto_2/JS/pages/CentralLoja.js
+++ Projeto_2/JS/pages/CentralLoja.js
@@ -1,4 +1,15 @@
 import { configurarHeader } from "../components/header.js";
+import { getUsuarioLogado } from "../core/auth.js";
+
+const usuario = getUsuarioLogado;
+const loja = usuario.loja;
+
+const Pagina = document.getElementById("MinhaLoja");
+
+Pagina.addEventListener("click", () => {
+        window.location.href = `PaginaLoja.html?id=${loja.id}`;
+    });
+
 
 document.addEventListener("DOMContentLoaded", () => {
     configurarHeader();
