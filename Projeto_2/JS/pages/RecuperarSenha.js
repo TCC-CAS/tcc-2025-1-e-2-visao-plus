56bef9a54c5bc6abb91f61edfbd40a0834882aa1
Gabriel <gabrieljoschem@gmail.com>
2026-03-13 20:51:04 -0300

    criação do método js de recuperação de senha

--- Projeto_2/JS/pages/RecuperarSenha.js
+++ Projeto_2/JS/pages/RecuperarSenha.js
@@ -1,9 +1,50 @@
 import { configurarHeader } from "../components/header2.js";
 import { API } from "../core/api.js";
 
-function EnviaEmail(usuario){
-    
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+
+async function testaNome(nome) {
+    const existe = await fetch(`${API}/usuarios/existe-nome`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(nome)
+        }
+    );
 }
+
+async function testaEmail(email) {
+    const existe = await fetch(`${API}/usuarios/existe-email`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(email)
+        }
+    );
+}
+
+async function EnviaEmail(usuario) {
+
+    await fetch(`${API}/usuarios/recuperarSenha`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(respostaCotacao)
+        }
+    );
+
+}
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("")
 
 
 
