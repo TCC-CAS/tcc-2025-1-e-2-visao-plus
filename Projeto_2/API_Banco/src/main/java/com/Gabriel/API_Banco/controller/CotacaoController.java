650845ea5ccefb8639914efbb62c5f2a8bb686eb
Gabriel <gabrieljoschem@gmail.com>
2026-03-13 20:48:11 -0300

    Criação do controller de cotação para resposta

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/controller/CotacaoController.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/controller/CotacaoController.java
@@ -3,11 +3,13 @@ package com.Gabriel.API_Banco.controller;
 import com.Gabriel.API_Banco.dto.CriarCotacaoDTO;
 import com.Gabriel.API_Banco.dto.ListarCotacoesDTO;
 import com.Gabriel.API_Banco.dto.ListarLentesDTO;
+import com.Gabriel.API_Banco.dto.ResponderCotacaoDTO;
 import com.Gabriel.API_Banco.model.Cotacao;
 import com.Gabriel.API_Banco.service.CotacaoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @CrossOrigin(origins = "*") // Permite requisições do frontend
@@ -29,5 +31,13 @@ public class CotacaoController {
     @GetMapping("/listarCotacoesPU/{idUsuario}")
     public ResponseEntity<List<ListarCotacoesDTO>> ListarPorUsuario(@PathVariable Long idUsuario) {
         return ResponseEntity.ok(cotacaoService.listarPorUsuario(idUsuario));
+    }
+
+    @PostMapping("/cotacoes/{id}/responder")
+    public Cotacao responder(@PathVariable Long id,
+                             @RequestBody ResponderCotacaoDTO dto,
+                             @RequestParam Long idLojaLogada) {
+
+        return cotacaoService.responderCotacao(id, dto, idLojaLogada);
     }
 }
