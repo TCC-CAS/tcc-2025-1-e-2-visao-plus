e8d02f55d1b826b46281ba86c3f89d866f5e8efe
Gabriel <gabrieljoschem@gmail.com>
2026-05-08 02:00:27 -0300

    DTO do envio de mensagens

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/EnviarMensagemDTO.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/EnviarMensagemDTO.java
@@ -1,4 +1,13 @@
 package com.Gabriel.API_Banco.dto;
 
+import lombok.Data;
+
+@Data
 public class EnviarMensagemDTO {
+
+    private Long idCotacao;
+    private Long idRemetente;
+    private String texto;
+
+    // getters e setters
 }
