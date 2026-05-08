eaff5bcb8b88646b460066b46ce9320018bde31f
Gabriel <gabrieljoschem@gmail.com>
2026-05-08 02:01:37 -0300

    Repositorio das mensagens de cotação

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/MensagemCotacaoRepository.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/MensagemCotacaoRepository.java
@@ -1,4 +1,13 @@
 package com.Gabriel.API_Banco.repository;
 
-public interface MensagemCotacaoRepository {
+import com.Gabriel.API_Banco.model.MensagemCotacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MensagemCotacaoRepository extends JpaRepository<MensagemCotacao, Long> {
+
+    List<MensagemCotacao> findByCotacaoIdOrderByEnviadoEmAsc(Long idCotacao);
 }
