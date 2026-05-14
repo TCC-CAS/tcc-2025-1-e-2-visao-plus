ac1eff55b1bf5936035c5707a1231cf741e01af2
Gabriel <gabrieljoschem@gmail.com>
2026-05-13 23:57:11 -0300

    If usuário logado básico para caso ele consiga entrar com o link independente

--- Projeto_2/JS/pages/CentralLoja.js
+++ Projeto_2/JS/pages/CentralLoja.js
@@ -5,7 +5,11 @@ import { getUsuarioLogado } from "../core/auth.js";
 document.addEventListener("DOMContentLoaded", () => {
     const usuario = getUsuarioLogado();
 
-
+    if (!usuario) {
+        alert("Você precisa estar logado para acessar o painel da loja");
+        window.location.href = "Login.html";
+        return;
+    }
 
 
 
