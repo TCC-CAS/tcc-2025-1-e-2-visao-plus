1031fb69b43fdbb3d355f5063ae75df365081aee
Gabriel <gabrieljoschem@gmail.com>
2026-05-13 23:51:32 -0300

    import do carrossel

--- Projeto_2/JS/pages/CentralLoja.js
+++ Projeto_2/JS/pages/CentralLoja.js
@@ -1,4 +1,5 @@
 import { configurarHeader } from "../components/header.js";
+import { montarCarrosselCotacoes } from "../components/CarrosselCotacoes.js";
 import { getUsuarioLogado } from "../core/auth.js";
 
 const usuario = getUsuarioLogado();
