3a326fc1f3b4ddae44208cfa4cea93a2955c30f0
Gabriel <gabrieljoschem@gmail.com>
2026-05-14 23:38:40 -0300

    imports

---
+++ Projeto_2/JS/components/CarrosselCotacoes.js
@@ -0,0 +1,3 @@
+import { getUsuarioLogado } from "../core/auth.js";
+import { listarCotacoesPorUsuario, listarCotacoesPorLoja, criarCardCotacao, chamarEstilizacao } from "../core/cotacoes.js";
+import { getLojaDoUsuario } from "../core/loja.js";
