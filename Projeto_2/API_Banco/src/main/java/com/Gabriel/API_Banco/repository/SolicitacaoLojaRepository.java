4575cad1078c01f500074f8a9b5316f95a1bb17d
Gabriel <gabrieljoschem@gmail.com>
2026-05-22 01:03:47 -0300

    Repositório para a solicitação da loja criado e configurado

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/SolicitacaoLojaRepository.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/SolicitacaoLojaRepository.java
@@ -1,4 +1,21 @@
 package com.Gabriel.API_Banco.repository;
 
-public interface SolicitacaoLojaRepository {
+import com.Gabriel.API_Banco.model.SolicitacaoLoja;
+import com.Gabriel.API_Banco.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SolicitacaoLojaRepository extends JpaRepository<SolicitacaoLoja, Long> {
+
+    boolean existsByCnpj(String cnpj);
+
+    boolean existsBySolicitante(Usuario solicitante);
+
+    Optional<SolicitacaoLoja> findBySolicitante(Usuario solicitante);
+
+    Optional<SolicitacaoLoja> findBySolicitanteId(Long idUsuario);
+
+    List<SolicitacaoLoja> findAllByOrderByDataSolicitacaoDesc();
 }
