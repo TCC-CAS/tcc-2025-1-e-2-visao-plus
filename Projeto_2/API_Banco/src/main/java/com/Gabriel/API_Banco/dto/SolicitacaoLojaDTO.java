e653787764405029b7c6ffc061729bd670d25a4a
Gabriel <gabrieljoschem@gmail.com>
2026-05-22 01:04:22 -0300

    DTO de abertura de solicitação de abertura de loja criada

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/SolicitacaoLojaDTO.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/SolicitacaoLojaDTO.java
@@ -1,4 +1,14 @@
 package com.Gabriel.API_Banco.dto;
 
+import lombok.Data;
+
+@Data
 public class SolicitacaoLojaDTO {
+    private Long idUsuario;
+    private String nome;
+    private String email;
+    private String cnpj;
+    private String cep;
+    private String endereco;
+    private String descricao;
 }
