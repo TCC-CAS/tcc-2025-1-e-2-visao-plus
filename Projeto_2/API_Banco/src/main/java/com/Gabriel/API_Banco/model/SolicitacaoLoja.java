b5c3c054a200cbe65e4a50a9b82e4fa70bbf8c29
Gabriel <gabrieljoschem@gmail.com>
2026-05-22 01:03:17 -0300

    Entidade da solicitação de loja criada.

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/model/SolicitacaoLoja.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/model/SolicitacaoLoja.java
@@ -1,4 +1,42 @@
 package com.Gabriel.API_Banco.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "tabela_solicitacao_loja")
 public class SolicitacaoLoja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_solicitacao_loja")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", nullable = false)
+    private Usuario solicitante;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cnpj", nullable = false, unique = true)
+    private String cnpj;
+
+    @Column(name = "cep")
+    private String cep;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "descricao", length = 500)
+    private String descricao;
+
+    @Column(name = "data_solicitacao")
+    private LocalDateTime dataSolicitacao = LocalDateTime.now();
 }
