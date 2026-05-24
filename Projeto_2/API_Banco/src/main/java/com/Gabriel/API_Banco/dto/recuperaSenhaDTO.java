303e3417b13c3d635513a2a94674784d2c9c0b51
Gabriel <gabrieljoschem@gmail.com>
2026-05-23 23:58:40 -0300

    dto de recuperação de senha para inclusão do email

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/recuperaSenhaDTO.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/dto/recuperaSenhaDTO.java
@@ -1,15 +1,9 @@
 package com.Gabriel.API_Banco.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class recuperaSenhaDTO  {
-
+public class recuperaSenhaDTO {
+    private String nome;
     private String email;
-
-
 }
