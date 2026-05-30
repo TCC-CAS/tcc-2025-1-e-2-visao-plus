40de6ae2666bd64d8b653cd1f415f7c627a723a8
Gabriel <gabrieljoschem@gmail.com>
2026-05-30 00:03:15 -0300

    Inclusão de colunas e variáveis na entidade da loja

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/model/Loja.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/model/Loja.java
@@ -22,30 +22,55 @@ public class Loja {
     @JsonBackReference
     private Usuario dono;
 
+    @Column(name = "razao_social")
+    private String razaoSocial;
+
     @Column(name = "nome")
     private String nome;
 
     @Column(name = "email")
     private String email;
 
-    @Column(name = "cnpj")
+    @Column(name = "cnpj", unique = true)
     private String cnpj;
 
     @Column(name = "cep")
     private String cep;
 
-    @Column(name= "endereco")
+    @Column(name = "logradouro")
+    private String logradouro;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "complemento")
+    private String complemento;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "uf")
+    private String uf;
+
+    @Column(name = "endereco")
     private String endereco;
 
     @Column(name = "descricao")
     private String descricao;
 
-    @OneToMany(mappedBy = "loja", cascade = CascadeType.REMOVE, orphanRemoval = true)
-    @JsonIgnore
-    private List<Produto> produtos;
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
 
     @Column(name = "url_foto")
     private String fotoUrl;
 
-
+    @OneToMany(mappedBy = "loja", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    @JsonIgnore
+    private List<Produto> produtos;
 }
