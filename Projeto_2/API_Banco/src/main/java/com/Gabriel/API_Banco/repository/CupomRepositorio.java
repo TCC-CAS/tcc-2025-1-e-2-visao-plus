046583d5e41a048c1fd6c8f1fd2b1781cbbc3b3a
Gabriel <gabrieljoschem@gmail.com>
2026-05-31 23:13:55 -0300

    Repos atualizados

--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/CupomRepositorio.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/CupomRepositorio.java
@@ -1,4 +1,25 @@
 package com.Gabriel.API_Banco.repository;
 
-public interface CupomRepositorio {
+import com.Gabriel.API_Banco.model.Cupom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface CupomRepositorio extends JpaRepository<Cupom, Long> {
+
+    List<Cupom> findByLojaIdAndDeletadoFalseOrderByCriadoEmDesc(Long lojaId);
+
+    List<Cupom> findByLojaIdAndAtivoTrueAndPublicoTrueAndDeletadoFalseAndDataValidadeAfter(
+            Long lojaId,
+            LocalDateTime agora
+    );
+
+    List<Cupom> findByAtivoTrueAndPublicoTrueAndDeletadoFalseAndDataValidadeAfter(
+            LocalDateTime agora
+    );
+
+    long countByLojaIdAndAtivoTrueAndDeletadoFalse(Long lojaId);
+
+    boolean existsByLojaIdAndCodigoIgnoreCaseAndDeletadoFalse(Long lojaId, String codigo);
 }
--- Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/CupomUsuarioRepositorio.java
+++ Projeto_2/API_Banco/src/main/java/com/Gabriel/API_Banco/repository/CupomUsuarioRepositorio.java
@@ -1,4 +1,19 @@
 package com.Gabriel.API_Banco.repository;
 
-public interface CupomUsuarioRepositorio {
+import com.Gabriel.API_Banco.model.CupomUsuario;
+import com.Gabriel.API_Banco.model.enums.StatusCupomUsuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CupomUsuarioRepositorio extends JpaRepository<CupomUsuario, Long> {
+
+    boolean existsByCupomIdAndUsuarioId(Long cupomId, Long usuarioId);
+
+    Optional<CupomUsuario> findByCupomIdAndUsuarioId(Long cupomId, Long usuarioId);
+
+    List<CupomUsuario> findByUsuarioIdOrderByResgatadoEmDesc(Long usuarioId);
+
+    long countByUsuarioIdAndStatus(Long usuarioId, StatusCupomUsuario status);
 }
